b61dab81e6b868727c4b9aa25cac044160e02d1a
nazir1362002 <nazirkhan1362002@gmail.com>
2026-03-29 22:44:36 +0600

    Problem 1 Solved

--- question1.dart
+++ question1.dart
@@ -1,21 +1,22 @@
 // Question 1: Basic Data Types & Functions (Difficulty: 1/5) ⭐
-/// EXPECTED OUTPUT:
-/// Name: John Doe, Age: 25, Height: 5.9, Is Student: true
-/// BMI: 22.5
-/// Grade: B
+// EXPECTED OUTPUT:
+// Name: John Doe, Age: 25, Height: 5.9, Is Student: true
+// BMI: 22.5
+// Grade: B
 
 // 1. Create variables of different data types: String, int, double, bool
-// TODO: Add your variables here
-String name = '';
-int age = 0;
-double height = 0;
-bool isStudent = false;
+// TO-DO: Add your variables here
+String name = 'John Doe';
+int age = 25;
+double height = 5.9;
+bool isStudent = true;
 
 // 2. Write a function called calculateBMI that takes weight (double) and height (double) as parameters and returns the BMI as a double
-// TODO: Implement the calculateBMI function
+// TO-DO: Implement the calculateBMI function
 double calculateBMI(double weight, double height) {
-  // TODO: Calculate BMI = weight / (height * height)
-  return 0;
+// TO-DO: Calculate BMI = weight / (height * height)
+  final calculateBMI = weight / (height * height);
+  return calculateBMI;
 }
 
 // 3. Write a function called getGrade that takes a score (int) and returns a grade (String) based on:
@@ -24,20 +25,29 @@ double calculateBMI(double weight, double height) {
 //    - 70-79: C
 //    - 60-69: D
 //    - Below 60: F
-// TODO: Implement the getGrade function
+// TO-DO: Implement the getGrade function
 String getGrade(int score) {
-  // TODO: Add your logic here
-  return '';
+  if (score >= 90) {
+    return 'A';
+  } else if (score >= 80) {
+    return 'B';
+  } else if (score >= 70) {
+    return 'C';
+  } else if (score >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
 }
 
 void main() {
-  // TODO: Initialize your variables with appropriate values
+  // TO-DO: Initialize your variables with appropriate values
 
-  // TODO: Calculate BMI and grade
-  var bmi = 0;
-  var grade = '';
+  // TO-DO: Calculate BMI and grade
+  var bmi = calculateBMI(89.9, 1.75);
+  var grade = getGrade(101);
 
-  // TODO: Use string interpolation to display the results as shown in expected output
+  // TO-DO: Use string interpolation to display the results as shown in expected output
   print('Name: $name, Age: $age, Height: $height, Is Student: $isStudent');
   print('BMI: $bmi');
   print('Grade: $grade');
